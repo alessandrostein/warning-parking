3dc817e553e46b77aa9284464f8e94d1ba7f2cc9
alessandrostein <alessandro@alessandrostein.com>
2015-06-19 23:47:41 -0300

    Config all ports with Digital

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 void main(){
+  ADCON1 = 0x07; // Configura todas portas como Digital
   // Configurações do Buzzy
   TRISC.RC1 = 0; // Configura como Saída
   PORTC.RC1 = 1; // Inicia Buzzy Desligado
