84ab24c226063528ffc4dd2385bbb49a3c0a9cbb
alessandrostein <alessandro@alessandrostein.com>
2015-06-19 23:33:00 -0300

    Config Leds

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 void main(){
   // Configurações do Buzzy
   TRISC.RC1 = 0; // Configura como Saída
-  PORTC.RC1 = 1; // Inicia Buzzy Desligado.
+  PORTC.RC1 = 1; // Inicia Buzzy Desligado
+  // Configurações do Led
+  TRISB = 0b00000000; // Configura como Saída
+  PORTB = 0b00000000; // Inicia todos os Leds Desligado
 }
